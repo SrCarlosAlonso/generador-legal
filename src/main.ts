258260cc81187356d97f457c8d5f5bf03dcca7e2
Carlos Alonso <cbm.alonso@gmail.com>
2025-02-10 08:20:50 -0500

    Enhance index.html structure and styling; remove unused files and refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+console.log('Hello World!')
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const $ = (id: string): HTMLElement | null => {
+  return document.getElementById(id);
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const DOM_ELEMENTS = {
+  "social": $("input-social"),
+  "nombre-comercial": $("input-comercial"),
+  "cif": $("input-cif"),
+  "direccion": $("input-direccion"),
+  "email": $("input-email"),
+  "website": $("input-website"),
+  "fecha": $("input-fecha"),
+}
+
+
+console.log(DOM_ELEMENTS);
+
+
