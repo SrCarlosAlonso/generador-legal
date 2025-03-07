cadf3b0f9226094124011bcad588bafbf33b4888
Carlos Alonso <cbm.alonso@gmail.com>
2025-03-07 17:16:21 -0500

    Refactor main.ts to improve DOM element handling; add event listeners for input changes and enhance error handling for element retrieval

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,42 @@
-console.log('Hello World!')
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM is ready!");
 
-const $ = (id: string): HTMLElement | null => {
-  return document.getElementById(id);
+  handleInputs(DOM_INPUTS);
+  console.log(DOM_ELEMENTS);
+});
+
+const $ = (id: string) => {
+  const el = document.getElementById(id);
+  if (!el) {
+    throw new Error(`Elemento ${id} no encontrado`);
+  }
+  return el;
+};
+
+const DOM_INPUTS = {
+  social: $("input-social") as HTMLInputElement,
+  nombreComercial: $("input-comercial") as HTMLInputElement,
+  cif: $("input-cif") as HTMLInputElement,
+  direccion: $("input-direccion") as HTMLInputElement,
+  email: $("input-email") as HTMLInputElement,
+  website: $("input-website") as HTMLInputElement,
+  fecha: $("input-fecha") as HTMLInputElement,
 };
 
 const DOM_ELEMENTS = {
-  "social": $("input-social"),
-  "nombre-comercial": $("input-comercial"),
-  "cif": $("input-cif"),
-  "direccion": $("input-direccion"),
-  "email": $("input-email"),
-  "website": $("input-website"),
-  "fecha": $("input-fecha"),
-  "btn-toggle-privacidad": $("btn-toggle-privacidad"),
-  "btn-toggle-legal": $("btn-toggle-legal"),
-  "txt-toggle-privacidad": $("txt-toggle-privacidad"),
-  "txt-toggle-legal": $("txt-toggle-legal"),
+  btnPrivacidad: $("btn-toggle-privacidad") as HTMLButtonElement,
+  btnLegal: $("btn-toggle-legal") as HTMLButtonElement,
+  txtPrivacidad: $("txt-toggle-privacidad") as HTMLDivElement,
+  txtLegal: $("txt-toggle-legal") as HTMLDivElement,
+};
+
+
+function listenerInput(input : HTMLInputElement) {
+  input.addEventListener("change", () => {
+    console.log(input.value);
+  });
 }
 
-
-console.log(DOM_ELEMENTS);
-
-
+function handleInputs(input : Object) {
+  Object.values(input).forEach((input) => listenerInput(input));
+}
