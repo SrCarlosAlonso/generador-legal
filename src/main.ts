810cd25f745ea29e16c1c98ff2819528c9cff8ae
Carlos Alonso <cbm.alonso@gmail.com>
2025-03-07 17:48:42 -0500

    Refactor index.html and main.ts to update preview section IDs for consistency; remove unused DOM_ELEMENTS logging and add comments for future functionality

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM is ready!");
 
   handleInputs(DOM_INPUTS);
-  console.log(DOM_ELEMENTS);
 });
 
 const $ = (id: string) => {
@@ -21,15 +20,12 @@ const DOM_INPUTS = {
   email: $("input-email") as HTMLInputElement,
   website: $("input-website") as HTMLInputElement,
   fecha: $("input-fecha") as HTMLInputElement,
-};
 
-const DOM_ELEMENTS = {
   btnPrivacidad: $("btn-toggle-privacidad") as HTMLButtonElement,
   btnLegal: $("btn-toggle-legal") as HTMLButtonElement,
   txtPrivacidad: $("txt-toggle-privacidad") as HTMLDivElement,
   txtLegal: $("txt-toggle-legal") as HTMLDivElement,
 };
-
 
 function listenerInput(input : HTMLInputElement) {
   input.addEventListener("change", () => {
@@ -40,3 +36,7 @@ function listenerInput(input : HTMLInputElement) {
 function handleInputs(input : Object) {
   Object.values(input).forEach((input) => listenerInput(input));
 }
+
+// 1. let's print the data after the input changed and the button generate clicked.
+// 2. handelButtons. when do you click on generate button. the program will save the data in a new object, then we take the new inforation objecto to generate the legal document.
+// 2.1 if the user clilc eb delete, all the data will be deleted.
