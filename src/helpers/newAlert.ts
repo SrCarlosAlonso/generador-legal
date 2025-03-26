f56783b84a0c9d44c28d0ef87075e2d854a93253
Carlos Alonso <cbm.alonso@gmail.com>
2025-03-25 21:53:26 -0400

    Enhance newAlert function to remove existing alerts before adding a new one and automatically dismiss alerts after 3 seconds

--- src/helpers/newAlert.ts
+++ src/helpers/newAlert.ts
@@ -2,12 +2,19 @@ import { DOM_ELEMENTS } from "./domElements";
 const { alertContainer } = DOM_ELEMENTS;
 
 export function newAlert(message: string, type: string) {
-const classAlert = `alert-${type}`;
-const alert = document.createElement("p");
-alert.id = "message-alert";
-alert.classList.add(classAlert);
-alert.innerText = message;
-alertContainer.appendChild(alert);
-};
+  const existingAlert = document.getElementById("message-alert");
+  if (existingAlert) {
+    existingAlert.remove();
+  }
 
+  const classAlert = `alert-${type}`;
+  const alert = document.createElement("p");
+  alert.id = "message-alert";
+  alert.classList.add(classAlert);
+  alert.innerText = message;
+  alertContainer.appendChild(alert);
 
+  setTimeout(() => {
+    alert.remove();
+  }, 3000);
+}
